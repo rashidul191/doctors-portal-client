f32a4694fbc706a50cd2d0594b920df85dff654d
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-07-04 16:43:36 +0600

    not found page desing done.

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h5>Error !!! Sorry page not found</h5>
-      <h1 className="text-lg">404</h1>
-      <Link to="/">
-        {" "}
-        <p>Back to Home</p>{" "}
-      </Link>
+    <div className=" sm:mt-20 md:mt-36 flex justify-center text-center items-center">
+      <div>
+        <h5 className="text-xl text-error">Error !!! Sorry page not found</h5>
+        <h1 className="text-6xl font-bold text-error my-3">404</h1>
+        <Link to="/" className="text-primary underline">
+          {" "}
+          <p>Back to Home</p>{" "}
+        </Link>
+      </div>
     </div>
   );
 };
