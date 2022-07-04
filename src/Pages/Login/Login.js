cba4407970d4d0d9d9cd674ce81b3de1fa31cbd7
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-07-04 13:02:23 +0600

    social login component like google login is done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import SocialLogin from "./SocialLogin/SocialLogin";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login page</h2>
+  return (
+    <section className="h-screen flex justify-center items-center">
+      
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-center text-secondary text-2xl font-bold">Login page</h2>
+         <form>
+         <input type="text" placeholder="Type here" class="input input-bordered input-md w-full max-w-xs" />
+         </form>
+
+
+          <div class="card-actions justify-end">
+            <button class="btn w-full ">Login</button>
+          </div>
+          <SocialLogin></SocialLogin>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Login;
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+
+const SocialLogin = () => {
+  const navigate = useNavigate();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if (loading) {
+    return <p>Loading........</p>;
+  }
+  if (user) {
+    navigate("/");
+  }
+  let errorElement;
+
+  if (error) {
+    errorElement = <p className="text-center text-error">{error?.message}</p>;
+  }
+
+  return (
+    <div className="text-center">
+      {errorElement}
+      <div class="divider">OR</div>
+      <button onClick={() => signInWithGoogle()} class="btn btn-outline w-full">
+        Continue With Google
+      </button>
+    </div>
+  );
+};
+
+export default SocialLogin;
